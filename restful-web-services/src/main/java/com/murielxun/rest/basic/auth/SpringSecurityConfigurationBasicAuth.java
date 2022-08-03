25b19408a700a809536112ab444e3a91782fbf6e
Murielxun <qianxunxun@outlook.com>
2022-08-04 00:17:49 +0800

    upload

---
+++ restful-web-services/src/main/java/com/murielxun/rest/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,19 @@
+package com.murielxun.rest.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				// allow any option request to any Uri
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated().and().httpBasic();
+	}
+
+}
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001, 'in28minutes', 'Learn JPA', CURRENT_DATE(), false);
+
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002, 'in28minutes', 'Learn Data JPA', CURRENT_DATE(), false);
+
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003, 'in28minutes', 'Learn MS', CURRENT_DATE(), false);
